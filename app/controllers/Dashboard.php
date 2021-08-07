243ca01237434da51ff1327c3e7e00a35b8a755a
ayrtonscott <msproof.app@gmail.com>
2021-08-07 18:32:06 -0300

    7.0.0 Deployed

---
+++ app/controllers/Dashboard.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * @copyright Copyright (c) 2021 AltumCode (https://altumcode.com/)
+ *
+ * This software is exclusively sold through https://altumcode.com/ by the AltumCode author.
+ * Downloading this product from any other sources and running it without a proper license is illegal,
+ *  except the official ones linked from https://altumcode.com/.
+ */
+
+namespace Altum\Controllers;
+
+use Altum\Middlewares\Authentication;
+
+class Dashboard extends Controller {
+
+    public function index() {
+
+        Authentication::guard();
+
+        /* Delete Campaign Modal */
+        $view = new \Altum\Views\View('campaign/campaign_delete_modal', (array) $this);
+        \Altum\Event::add_content($view->run(), 'modals');
+
+        /* Create Campaign Modal */
+        $view = new \Altum\Views\View('campaign/create_campaign_modal', (array) $this);
+        \Altum\Event::add_content($view->run(), 'modals');
+
+        /* Update Campaign Modal */
+        $view = new \Altum\Views\View('campaign/update_campaign_modal', (array) $this);
+        \Altum\Event::add_content($view->run(), 'modals');
+
+        /* Custom Branding Campaign Modal */
+        if($this->user->plan_settings->custom_branding) {
+            $view = new \Altum\Views\View('campaign/custom_branding_campaign_modal', (array)$this);
+            \Altum\Event::add_content($view->run(), 'modals');
+        }
+
+        /* Pixel Modal */
+        $view = new \Altum\Views\View('campaign/campaign_pixel_key_modal', (array) $this);
+        \Altum\Event::add_content($view->run(), 'modals');
+
+        /* Prepare the filtering system */
+        $filters = (new \Altum\Filters(['is_enabled'], ['name', 'domain'], ['name', 'datetime']));
+
+        /* Prepare the paginator */
+        $total_rows = database()->query("SELECT COUNT(*) AS `total` FROM `campaigns` WHERE `user_id` = {$this->user->user_id} {$filters->get_sql_where()}")->fetch_object()->total ?? 0;
+        $paginator = (new \Altum\Paginator($total_rows, $filters->get_results_per_page(), $_GET['page'] ?? 1, url('dashboard?' . $filters->get_get() . '&page=%d')));
+
+        /* Get the campaigns list for the user */
+        $campaigns = [];
+        $campaigns_result = database()->query("SELECT * FROM `campaigns` WHERE `user_id` = {$this->user->user_id} {$filters->get_sql_where()} {$filters->get_sql_order_by()} {$paginator->get_sql_limit()}");
+        while($row = $campaigns_result->fetch_object()) $campaigns[] = $row;
+
+        /* Prepare the pagination view */
+        $pagination = (new \Altum\Views\View('partials/pagination', (array) $this))->run(['paginator' => $paginator]);
+
+        /* Prepare the View */
+        $data = [
+            'campaigns' => $campaigns,
+            'campaigns_total' => $total_rows,
+            'pagination' => $pagination,
+            'filters' => $filters,
+        ];
+
+        $view = new \Altum\Views\View('dashboard/index', (array) $this);
+
+        $this->add_view_content('content', $view->run($data));
+
+    }
+
+}
---
+++ update/info.php
@@ -0,0 +1,13 @@
+<?php
+
+define('NEW_PRODUCT_VERSION', '7.0.0');
+define('NEW_PRODUCT_CODE', '700');
+
+$updates = [
+    '500',
+    '600',
+    '610',
+    '620',
+    '630',
+    '700',
+];
