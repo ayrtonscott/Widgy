232e0ec5036ceb014dc088ee5c0a85d527a5aca6
ayrtonscott <msproof.app@gmail.com>
2021-12-13 21:22:34 -0300

    Config applied.

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 /* Configuration of the site */
-define('DATABASE_SERVER',   '');
-define('DATABASE_USERNAME', '');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME',     '');
-define('SITE_URL',          '');
+define('DATABASE_SERVER',   'localhost');
+define('DATABASE_USERNAME', 'ayrton');
+define('DATABASE_PASSWORD', 'ayrton123');
+define('DATABASE_NAME',     'cartelitos.app');
+define('SITE_URL',          'http://cartelitos.ddns.net/');
