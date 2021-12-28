1f94bfe2bd59f04fb828a5f684046bbfc069fe67
ayrtonscott <msproof.app@gmail.com>
2021-12-28 04:20:33 -0300

    Agregamos API KEY TN SECRET Y TN CLIENT

--- config.php
+++ config.php
@@ -6,3 +6,7 @@ define('DATABASE_USERNAME', 'ayrton');
 define('DATABASE_PASSWORD', 'ayrton123');
 define('DATABASE_NAME',     'cartelitos.app');
 define('SITE_URL',          'http://cartelitos.ddns.net/');
+// Estas variables han sido agregadas manualmente.
+define('SR_ADMIN_API_KEY',   '1337cambiame1337cambiame1337camb');
+define('TN_CLIENT_SECRET',   '5YOPBhLMIsPq2gtbbgR5VzU43GvjgpnEIZ1TpglLzaAez1Pl');
+define('TN_CLIENT_ID',   '2174');
