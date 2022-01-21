adf858a4f8c487d540371ab57f66b5e44e5d56c7
ayrtonscott <msproof.app@gmail.com>
2022-01-21 11:55:05 -0300

    Lang y imagenes

--- themes/widgy/views/wrapper.php
+++ themes/widgy/views/wrapper.php
@@ -120,7 +120,7 @@
 
         </div>
 
-        <a class="navbar-logo" href="Dashboard.Default.html">
+        <a class="navbar-logo" href="dashboard">
             <span class="logo d-none d-xs-block"></span>
             <span class="logo-mobile d-block d-xs-none"></span>
         </a>
