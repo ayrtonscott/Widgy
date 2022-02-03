357cad429934d2e9776e8981072fbbc8b7ca1b01
ayrtonscott <msproof.app@gmail.com>
2022-02-03 04:34:09 -0300

    Intercom en Index

--- themes/widgy/views/index/index.php
+++ themes/widgy/views/index/index.php
@@ -540,26 +540,16 @@
 
 <?php // * Agregado 2/2/22 en 10.0.0 - (Agregamos Intercom) INICIO. ?>
 <script>
-        window.intercomSettings = {
-            app_id: "nds4bz0l",
-            name: "<?= isset($this->user->name) ? $this->user->name : "" ?>",
-            language: "<?= isset($this->user->language) ? $this->user->language : "" ?>",
-            user_id: "<?= isset($this->user->user_id) ? $this->user->user_id : "" ?>",
-            email: "<?= isset($this->user->email) ? $this->user->email : "" ?>",
-            user_hash: "<?= isset($hash) ? $hash : "" ?>",
-            executive: "<?= isset($this->user->plan_settings->no_ads) ? $this->user->plan_settings->no_ads : "" ?>",
-            created_at: "<?= isset($this->user->datetime) ? strtotime($this->user->datetime) : "" ?>",
-            plan: "<?= isset($this->user->plan_id) ? $this->user->plan_id : "" ?>",
-            trial: "<?= isset($this->user->plan_trial_done) ? $this->user->plan_trial_done : "" ?>",
-            current_impressions: "<?= isset($this->user->current_month_notifications_impressions) ? $this->user->current_month_notifications_impressions : "" ?>",
-            impressions_limit: "<?= isset($this->user->plan_settings->notifications_impressions_limit) ? $this->user->plan_settings->notifications_impressions_limit :"" ?>",
-            Vencimiento: "<?= isset($this->user->plan_expiration_date) ? $this->user->plan_expiration_date : "" ?>"
-        };
-    </script>
-    <script>
-    // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/nds4bz0l'
-    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/nds4bz0l';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-    </script>
+  window.intercomSettings = {
+    api_base: "https://api-iam.intercom.io",
+    app_id: "nds4bz0l"
+  };
+</script>
+
+<script>
+// We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/nds4bz0l'
+(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/nds4bz0l';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+</script>
     <?php // * Agregado 2/2/22 en 10.0.0 - (Agregamos Intercom) FIN. ?>
 
 <?php \Altum\Event::add_content(ob_get_clean(), 'javascript') ?>
