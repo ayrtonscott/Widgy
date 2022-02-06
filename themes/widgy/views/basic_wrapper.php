0cc74909f58ba2d4aa9b848580373fc42e00a1f4
ayrtonscott <msproof.app@gmail.com>
2022-02-05 21:57:45 -0300

    Fix intercom borra datos

--- themes/widgy/views/basic_wrapper.php
+++ themes/widgy/views/basic_wrapper.php
@@ -111,19 +111,7 @@
 <?php // * Agregado 21/01 en 10.0.0 - (Agregamos Intercom) INICIO. ?>
 <script>
         window.intercomSettings = {
-            app_id: "nds4bz0l",
-            name: "<?= isset($this->user->name) ? $this->user->name : "" ?>",
-            language: "<?= isset($this->user->language) ? $this->user->language : "" ?>",
-            user_id: "<?= isset($this->user->user_id) ? $this->user->user_id : "" ?>",
-            email: "<?= isset($this->user->email) ? $this->user->email : "" ?>",
-            user_hash: "<?= isset($hash) ? $hash : "" ?>",
-            executive: "<?= isset($this->user->plan_settings->no_ads) ? $this->user->plan_settings->no_ads : "" ?>",
-            created_at: "<?= isset($this->user->datetime) ? strtotime($this->user->datetime) : "" ?>",
-            plan: "<?= isset($this->user->plan_id) ? $this->user->plan_id : "" ?>",
-            trial: "<?= isset($this->user->plan_trial_done) ? $this->user->plan_trial_done : "" ?>",
-            current_impressions: "<?= isset($this->user->current_month_notifications_impressions) ? $this->user->current_month_notifications_impressions : "" ?>",
-            impressions_limit: "<?= isset($this->user->plan_settings->notifications_impressions_limit) ? $this->user->plan_settings->notifications_impressions_limit :"" ?>",
-            Vencimiento: "<?= isset($this->user->plan_expiration_date) ? $this->user->plan_expiration_date : "" ?>"
+            app_id: "nds4bz0l"
         };
     </script>
 
