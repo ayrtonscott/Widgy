bbfd5ac7726945b9b6c6640f94627d53f1eed2ac
ayrtonscott <widgy.app@gmail.com>
2022-03-10 13:24:36 -0300

    Deleted ALL from 13.1.0

--- update/sql/1310.sql
+++
@@ -1,2 +0,0 @@
-UPDATE `settings` SET `value` = '{\"version\":\"13.1.0\", \"code\":\"1310\"}' WHERE `key` = 'product_info';
-
--- update/sql/600.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `settings` SET `value` = '{\"version\":\"6.0.0\", \"code\":\"600\"}' WHERE `key` = 'product_info';
